641acdbc334f1bda58e786bf6150560adf2dd40a
Teraaret <raiiliiat@yandex.ru>
2020-01-21 18:55:53 +0500

    Level parse

--- game.py
+++ game.py
@@ -1,5 +1,26 @@
 import pygame
 import math
+from PIL import Image
+
+original = Image.open("levels/00.png")   #create the pixel map
+# print("The size of the Image is: ")
+# print(original.format, original.size, original.mode)
+
+original_rgb = original.convert('RGB')
+game_matrix = []
+
+for i in range(25):    # for every col:
+    for j in range(25):    # For every row
+        r, g, b = original_rgb.getpixel((i, j))
+        # print(r, g, b)
+        if( r == 0 and g == 0 and b == 0 ):
+            # print("wall!")
+            game_matrix.append({'x': i, 'y': j, 'type': 'wall'})
+        if( r == 255 and g == 255 and b == 255 ):
+            game_matrix.append({'x': i, 'y': j, 'type': 'air'})
+        #     game_matrix[i,j] = {'x': i, 'y': j, 'type': 'stone'}
+
+print(game_matrix)
 
 player_x = 200
 player_y = 400
@@ -8,6 +29,8 @@ player_height = 25
 speed = 5
 isJump = True
 jumpCount = 10
+
+joystick = False
 
 game_width = 500
 game_height = 500
@@ -22,8 +45,10 @@ win = pygame.display.set_mode((game_width,game_height))
 
 pygame.display.set_caption("Jump game")
 
-joystick = pygame.joystick.Joystick(0)
-joystick.init()
+if pygame.joystick.get_count() > 0:
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
+
 
 run = True
 while run:
@@ -46,20 +71,21 @@ while run:
     # if keys[pygame.K_UP] and player_y > game_top_border:
     #     player_y -= speed
 
-    # Ускорение O
-    if(joystick.get_button(0) == 1):
-        speed = 10
-    else:
-        speed = 5
+    if joystick:
+        # Ускорение O
+        if(joystick.get_button(0) == 1):
+            speed = 10
+        else:
+            speed = 5
+        # Прыжок X
+        if(joystick.get_button(1) == 1):
+            isJump = True
+        # Стик по горизонтали
+        if(joystick.get_axis(0) > 0) and player_x < game_right_border:
+            player_x += math.ceil(speed * joystick.get_axis(0))
+            if(joystick.get_axis(0) < 0) and player_x > game_left_border:
+                player_x += math.ceil(speed * joystick.get_axis(0))
 
-    # Прыжок X
-    if(joystick.get_button(1) == 1):
-        isJump = True
-    # Стик по горизонтали
-    if(joystick.get_axis(0) > 0) and player_x < game_right_border:
-        player_x += math.ceil(speed * joystick.get_axis(0))
-    if(joystick.get_axis(0) < 0) and player_x > game_left_border:
-        player_x += math.ceil(speed * joystick.get_axis(0))
 
     # Падение
     if(isJump):
